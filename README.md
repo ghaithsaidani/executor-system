2522b551e7a877c375407fd94ba156f329b46478
ghaithsaidani <ghaith.saidani@sesame.com.tn>
2026-03-10 15:43:15 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Executor Jetbrains Test
---
+++ src/test/java/org/jetbrains/executor/ExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jetbrains.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
